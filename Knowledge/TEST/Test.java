be11d6a0b74dc91ef327a39a7b265db9a957fa04
chiayhon <571617842@qq.com>
2019-10-24 14:53:04 +0800

    更新基础

---
+++ Knowledge/TEST/Test.java
@@ -0,0 +1,18 @@
+package TEST;
+
+/**
+ * @author Chiayhon
+ * @create 2019 - 10 - 24
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println("17模以3的值为：" + Math.floorMod(17, 3));
+        System.out.println("17除以3的余为：" + 17 % 3);
+        System.out.println("-17模以-3的值为：" + Math.floorMod(-17, -3));
+        System.out.println("-17除以-3的余为：" + -17 % -3);
+        System.out.println("-17模以3的值为：" + Math.floorMod(-17, 3));
+        System.out.println("-17除以3的余为：" + -17 % 3);
+        System.out.println("17模以-3的值为：" + Math.floorMod(17, -3));
+        System.out.println("17除以-3的余为：" + 17 % -3);
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # 面经
-## 一. java基础
-
+## 一. java SE部分
+[java基础](/Knowledge/java基础/java基础.md)
 ## 二. 多线程
 
 ## 三. spring
@@ -13,7 +13,9 @@
 
 ## 七. 网络基础
 
-[1. 网络模型](/Knowledge/计算机网络/网络模型.md)
-[2. 传输层](/Knowledge/计算机网络/传输层.md)
-[3. 应用层](/Knowledge/计算机网络/传输层.md)
+[1. 计算机概述](/Knowledge/计算机网络/计算机概述.md)
+
+[2. 物理层](/Knowledge/计算机网络/传输层.md)
+
+[3. 应用层](/Knowledge/计算机网络/应用层.md)
 
