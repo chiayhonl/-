a712a6b3b31ac32847fe82fe5d8117c8a6c6912a
chiayhon <571617842@qq.com>
2019-11-17 22:51:37 +0800

    update

--- Knowledge/TEST/ConCurrency/concurrency_basic/Concurrency7.java
+++ Knowledge/TEST/ConCurrency/concurrency_basic/Concurrency7.java
@@ -5,4 +5,72 @@ package TEST.ConCurrency.concurrency_basic;
  * @create 2019 - 10 - 31
  */
 public class Concurrency7 {
+
+    public static void main(String[] args) {
+        Task task = new Task();
+
+        task.excute(() -> {
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        task.shutDown(10_000);
+    }
+}
+
+class Task{
+
+    boolean flag = true;
+
+    Thread ExecutionThread;
+
+    long i;
+
+    public void excute(Runnable task){
+        ExecutionThread = new Thread(){
+            @Override
+            public void run() {
+                Thread DaemonThread = new Thread(task);
+
+                DaemonThread.setDaemon(true);
+
+                DaemonThread.start();
+
+                try {
+                    DaemonThread.join();
+                    flag = false;
+                    System.out.println("任务结束");
+                } catch (InterruptedException e) {
+                    System.out.println("执行线程的阻塞被中断");
+                }
+            }
+        };
+
+        ExecutionThread.start();
+    }
+
+    public void shutDown(long mills){
+        long currentTime = System.currentTimeMillis();
+        i = mills / 1000;
+        while (flag){
+            if(System.currentTimeMillis() - currentTime >= mills) {
+                System.out.println("超时线程未结束，将强制终止");
+                ExecutionThread.interrupt();
+                break;
+            }
+
+            try {
+                System.out.println(i--);
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.out.println("执行线程被中断");
+                break;
+            }
+        }
+
+        flag = false;
+    }
 }
--- Knowledge/TEST/test.java
+++ Knowledge/TEST/test.java
@@ -1,6 +1,10 @@
 package TEST;
 
+import java.util.HashSet;
+import java.util.TreeMap;
+
 public class test{
-
-
+    public static void main(String[] args) {
+        TreeMap
+    }
 }
